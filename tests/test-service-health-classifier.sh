c565daeb171a26cbab44ce6dbd4e17f0e2d56c7f
Andris Rožkalns <277435981+rozkalnsandris@users.noreply.github.com>
2026-09-20 22:11:06 +0200

    test: cover bounded missing startup state

--- tests/test-service-health-classifier.sh
+++ tests/test-service-health-classifier.sh
@@ -19,6 +19,8 @@ assert_class PASS required running required none running healthy 600 180 2
 assert_class TRANSIENT required running required none running starting 30 180 2
 assert_class FAIL_PERSISTENT required running required none running starting 180 180 2
 assert_class FAIL_PERSISTENT required running required none running unhealthy 600 180 2
+assert_class TRANSIENT required running required none missing none 0 180 2
+assert_class FAIL_PERSISTENT required running required none missing none 180 180 2
 assert_class FAIL_PERSISTENT required running absent http exited none 600 120 1
 assert_class TRANSIENT required running absent http running none 30 120 1
 assert_class FAIL_PERSISTENT required running absent http running none 120 120 1
